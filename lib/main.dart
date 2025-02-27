aeda5a3505210729604088afdf917f489b1ea632
Toretay <abdymvzhit@gmail.com>
2025-02-27 10:30:17 +0500

    connected with websocket to the server w/ messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,8 +22,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final TextEditingController _controller = TextEditingController();
+  final _channel = WebSocketChannel.connect(
+    Uri.parse(
+      'wss://echo.websocket.events',
+    ), // just used to send and retrieve the data (example)
+  );
+  @override
+  void dispose() {
+    _controller.dispose();
+    _channel.sink.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Form(
+                child: TextFormField(
+                  controller: _controller,
+                  decoration: InputDecoration(hintText: "Add your text here"),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              StreamBuilder(
+                stream: _channel.stream,
+                builder: (context, snapshot) {
+                  return Text(
+                    snapshot.hasData &&
+                            snapshot.data !=
+                                "echo.websocket.events sponsored by Lob.com"
+                        ? "Your text:\n${snapshot.data}"
+                        : "",
+                    style: TextStyle(fontSize: 24.0),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_controller.text.isNotEmpty) {
+            _channel.sink.add(_controller.text);
+            _controller.clear();
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
